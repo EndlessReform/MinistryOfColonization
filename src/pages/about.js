664043b36c717e309feac207855b55ac5f206ab3
Jacob Keisling <jlowellkeisling@gmail.com>
2021-03-21 02:51:37 -0500

    Final typo fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -51,7 +51,7 @@ class About extends React.Component {
             </blockquote>
             </Fader>
             <h3><Text>Our History</Text></h3>
-            <p><Text>Our department was founded in 2053 to advance settlements in Phobos, Deimos, and the Asteroid Belt. However, after groundbreaking developments in molecular nanotechnology during the mid 2060s, intergalactic colonization for the first time became technologically feasible. Since then, the MoC has been the only governmental body in the Solar System to spread human colonies across the Solar System.</Text></p> 
+            <p><Text>Our department was founded in 2053 to advance settlements in Phobos, Deimos, and the Asteroid Belt. However, after groundbreaking developments in molecular nanotechnology during the mid 2060s, intergalactic colonization for the first time became technologically feasible. Since then, the MoC has been the only governmental body in the Solar System to spread human colonies across the Milky Way.</Text></p> 
             <p><Text>Our numerous technical accomplishments include compiling a comprehensive guide to human science and technology in just five years with help from GPT-8 and crowdsourced volunteers, building the BFLA (Big F▒▒▒  Laser Array), a 100GW laser installation 10x more powerful than current state-of-the-art laser sail cargo terminals, to propel our probes, and successfully assembling a modern chip fabrication fab from scratch in just 36 hours with molecular nanotech. Our first test colony is currently replicating on Charon, and we await first contact within five years. Since first launch last year, we've sent over 100 candidate probes to inhabitable planets in a 50 light-year radius.</Text></p>
           </Secuence>
         </article>
